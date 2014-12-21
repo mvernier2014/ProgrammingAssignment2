4801630141b42bde0be309e2c631b6fb6f39873c
mvernier2014 <mvernier@umich.edu>
2014-12-21 14:29:03 -0500

    Programming Assignment #2
    Commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+# This first function will take the target matrix (x) and put it into cached
+# memory along with its inverse to be  retrieved later and compared
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # Create Set function to put target matrix in cached memory
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  # Create Get function to put the inverse matrix in cached memory
+  
+  get <-function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get, 
+       setinverse = setinverse, 
+       getinverse = getinverse)
+  
 }
 
-
-## Write a short comment describing this function
+# This second function compares the current matrix to the cached version,
+# and if the two are identical, display the cached inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  # If the current matrix is null, retrieve the cached version
+  
+  m < x$getinverse()
+  if(!is.null(m)) {
+    message ("Getting cached data")
+    return(m)
+  }
+  
+  # If the current matrix is not null, invert the matrix and display the
+  # solution
+  
+  data <- x$get()
+  m <- solve(x)
+  x$setinverse(m)
+  m
+  
 }
